00a2ac27a5b4d7faaeb5cb52323ff820adb35cf6
Чураева Надежда Эдуардовна <Churaeva.N.Ed@sberbank.ru>
2020-08-09 15:33:53 +0300

    task docker

---
+++ task17/src/main/java/otus/nchuraeva/spring/task17/config/ApplicationWebConfig.java
@@ -0,0 +1,52 @@
+package otus.nchuraeva.spring.task17.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+
+@EnableWebMvc
+@Configuration
+public class ApplicationWebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/webjars/**",
+                "/img/**",
+                "/css/**",
+                "/js/**")
+                .addResourceLocations(
+                        "classpath:/META-INF/resources/webjars/",
+                        "classpath:/static/img/",
+                        "classpath:/static/css/",
+                        "classpath:/static/js/");
+    }
+
+    @Bean
+    public ITemplateResolver templateResolver() {
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        resolver.setPrefix("templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setCacheable(true);
+        return resolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine(){
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+}
+
---
+++ task17/src/main/java/otus/nchuraeva/spring/task17/service/SubscriptionServiceImpl.java
@@ -0,0 +1,60 @@
+package otus.nchuraeva.spring.task17.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+import otus.nchuraeva.spring.task17.document.Subscription;
+import otus.nchuraeva.spring.task17.repository.SubscriptionRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class SubscriptionServiceImpl implements SubscriptionService {
+
+    private final SubscriptionRepository subscriptionRepository;
+    private final MongoTemplate mongoTemplate;
+
+    @Override
+    public List<Subscription> findAllSubscriptions(String userId) {
+        return subscriptionRepository.findAllByUserId(userId);
+    }
+
+    @Override
+    public void updateSubscription(String subscriptionId, Subscription updateSubscription) {
+        Query query = Query.query(Criteria.where("id").is(subscriptionId));
+        Update update = new Update();
+        update.set("price", updateSubscription.getPrice());
+        update.set("service.name", updateSubscription.getService().getName());
+        update.set("service.link", updateSubscription.getService().getLink());
+        update.set("service.category.name", updateSubscription.getService().getCategory().getName());
+
+        mongoTemplate.updateFirst(query, update, Subscription.class);
+    }
+
+    @Override
+    public void deleteSubscription(String subscriptionId) {
+        Subscription subscription = this.findSubscriptionById(subscriptionId);
+        subscriptionRepository.delete(subscription);
+    }
+
+    @Override
+    public void addSubscription(Subscription subscription) {
+        subscriptionRepository.save(subscription);
+    }
+
+    @Override
+    public Subscription findSubscriptionById(String subscriptionId) {
+        return subscriptionRepository.findById(subscriptionId)
+                .orElseThrow(() -> new IllegalArgumentException("Subscription not found with id: " + subscriptionId));
+    }
+
+    @Override
+    public List<Subscription> findAllSubscriptions() {
+        return subscriptionRepository.findAll();
+    }
+}
